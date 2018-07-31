8fc6a06c2a5bf8c0a09866e55d19bf317eae8e7d
technoir9 <roonie9@wp.pl>
2018-07-31 15:04:46 +0200

    Add filter functions to functional.js

--- functional.js
+++ functional.js
@@ -18,7 +18,7 @@ console.log(imperativeUrls(states));
 
 // urls: Functional version
 function functionalUrls(elements) {
-  return states.map(element => urlify(element));
+  return elements.map(element => urlify(element));
 }
 console.log(functionalUrls(states));
 
@@ -26,3 +26,36 @@ console.log(functionalUrls(states));
 function fullUrls(elements) {
   return elements.map(element => `https://example.com/${urlify(element)}`);
 }
+
+// Filter
+// singles: Imperative version
+function imperativeSingles(elements) {
+  let singles = [];
+  elements.forEach(element => {
+    if (element.split(/\s+/).length === 1) {
+      singles.push(element);
+    }
+  });
+  return singles;
+}
+console.log(imperativeSingles(states));
+
+// singles: Functional version
+function functionalSingles(elements) {
+  return elements.filter(element => element.split(/\s+/).length === 1);
+}
+console.log(functionalSingles(states));
+
+// Returns an array of strings which include the substring 'Dakota'
+function includesDakota(elements) {
+  return elements.filter(element => element.includes("Dakota"));
+}
+console.log(includesDakota(states));
+
+// doubles: Functional version
+// Returns an array of strings which by splitting on whitespace turn into
+// arrays of two elements
+function functionalDoubles(elements) {
+  return elements.filter(element => element.split(/\s+/).length === 2);
+}
+console.log(functionalDoubles(states));
